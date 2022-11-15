b7b16c47ce6e5babcbca028f68deafaeb6420723
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2022-11-14 23:48:38 -0800

    Setup vdf parsing so Steam libraries can be found

--- launcher/setup.sh
+++ launcher/setup.sh
@@ -12,19 +12,23 @@ searchFS () {
     # $1 is file/folder name to search for
     # $2 is criteria the path must follow to result in true, optional
     # $2 must be a function that sets `returnPath` variable
-    local results="$(find $USER_HOME/ -name $1)"
+    returnPath=""
+    local results="$(find $2 -name $1)"
     local results=$(echo $results | sed "s/\ \//\&\&\//g") # replace ' /' with '&&/'
     local results=$(echo $results | sed "s/\ /\#\#/g") # replace ' ' with '##'
     local results=$(echo $results | sed "s/\&\&/\ /g") # replace '&&' with ' '
     read -a searchArr <<< $results
     for path in ${searchArr[@]}; do
         path=$(echo $path | sed "s/\#\#/\\\ /g") # replace '##' with '\ '
-        if [[ "$2" != "" ]]; then
-            "$2" "$path"
+        if [[ "$3" != "" ]]; then
+            "$3" "$path"
         else
-            returnPath=$path
+            returnPath="$path"
         fi
-        
+
+        if [[ "$returnPath" != "" ]]; then
+            return
+        fi
     done
 }
 
@@ -35,39 +39,85 @@ searchFS () {
 #         returnPath="$path"
 #     fi
 # }
-# searchFS 284160 isPrefix
+# searchFS 284160 "$USER_HOME/" isPrefix
 # pfx="${returnPath}/pfx"
 
-# # Find Steam executable
-# steam=$(which steam)
-# if [ "$steam" == "" ]; then
-#     searchFS steam.sh
-#     steam="$returnPath"
-# fi
-# if [ "$steam" == "" ]; then
-#     searchFS Steam.exe
-#     steam="$returnPath"
-# fi
+# Find Steam executable
+searchFS steam.sh "$USER_HOME/"
+steam="$returnPath"
+steamDir=$(echo "$steam" | sed "s/\/steam.sh$//g")
 
-# Find Proton executable
-isProtonExp () {
-    path="$1"
-    if [[ "$path" == *"Proton\ -\ Experimental/proton"* ]]; then
-        returnPath="$path"
-    fi
-}
-isProtonGE () {
-    path="$1"
-    if [[ "$path" == *"Proton\ -\ Experimental/proton"* ]]; then
-        returnPath="$path"
-    fi
-}
-searchFS proton isProtonExp
-proton="$returnPath"
-if [ "$proton" == "" ]; then
-    searchFS proton isProton
-    steam="$returnPath"
-fi
-echo "proton is: $proton"
+# # Find Proton executable
+# isProtonExp () {
+#     path="$1"
+#     if [[ "$path" == *"Proton\ -\ Experimental/proton" ]]; then
+#         returnPath="$path"
+#     fi
+# }
+# searchFS proton "$USER_HOME/" isProtonExp
+# proton="$returnPath"
+
 
 # Find BeamNG executable
+
+# echo "$steamDir/config/libraryfolders.vdf"
+# libraryfolders=$(cat "$steamDir/config/libraryfolders.vdf")
+# libraryfolders=$(echo $libraryfolders | sed "s/\ /\#\#/g") # replace ' ' with '##'
+# for line in $(grep -oE '"path"\s*"/.+"'); do
+#     echo before $line
+#     path=$(echo "$line" | grep -oE '/\S+[^"]')
+#     echo after $path
+#     if [[ "$line" =~ "/" ]]; then
+#         echo $path
+#     fi
+
+# done
+inQuote="0"
+isGoingToBePath="0"
+isPath="0"
+word=""
+steamLibraries="" # Delimited by ' ', with literal ' ' replaced represented as '##'
+while read char; do
+    if [[ "$char" == '"' ]]; then
+        if [[ "$inQuote" == "0" ]]; then
+            inQuote="1"
+            if [[ "$isGoingToBePath" == "1" ]]; then
+                isGoingToBePath="0"
+                isPath="1"
+            fi
+        else
+            inQuote="0"
+            word=""
+            if [[ "$isPath" == "1" ]]; then
+                isPath="0"
+                steamLibraries="${steamLibraries} "
+            fi
+        fi
+    elif [[ "$isPath" == "1" ]]; then
+        if [[ "$char" == "" ]]; then # if char is a space (shown in code as empty string)
+            steamLibraries="${steamLibraries}##"
+        else
+            steamLibraries="${steamLibraries}${char}"
+        fi
+    elif [[ "path" =~ "$char" ]]; then
+        word="${word}${char}"
+        if [[ "$word" == "path" ]]; then
+            isGoingToBePath="1"
+        elif [[ ! "path" =~ "$word" ]]; then
+            word=""
+        fi
+    fi
+    ## For debugging, uncomment this
+    # echo $char inQuote $inQuote isGoingToBePath $isGoingToBePath isPath $isPath word $word
+done <<< "$(grep -oE . $steamDir/config/libraryfolders.vdf)"
+
+read -r -a steamLibrariesArr <<< "$steamLibraries"
+
+for steamLib in "${steamLibrariesArr[@]}"; do
+    steamLib=$(echo $steamLib | sed "s/\#\#/\\\ /g") # Replace '##' with '\ '
+    echo $steamLib
+done
+# searchFS BeamNG.drive.exe "$USER_HOME/"
+# beamng="$returnPath"
+
+# echo "beamng is: $beamng"
